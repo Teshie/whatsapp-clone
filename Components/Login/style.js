82bbef980597b59cb5456de41fab4b56789756c3
Teshome Ayechiluhem <teshome@ayechiluhem>
2021-08-07 12:22:27 +0300

    style login component

---
+++ Components/Login/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+export const Head = styled.div``;
+export const LoginContainer = styled.div``;
+export const Logo = styled.img`
+  height: 200px;
+  width: 200px;
+`;
